daf14d86c9675a5cc67b574ec92b902ea1af8cfc
Albin <83800790+Albindeveloer@users.noreply.github.com>
2021-07-22 22:35:46 +0530

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,104 @@
+const { response } = require("express");
+var express = require("express");
+const session = require("express-session");
+const { helpers } = require("handlebars");
+const { ProxyAuthenticationRequired } = require("http-errors");
+const { restart } = require("nodemon");
+var router = express.Router();
+var productHelpers = require("../helpers/product-helpers");
+var userHelpers = require("../helpers/user-helpers");
+
+const verifyLogin = (req, res, next) => {
+  if (req.session.loggedIn) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+};
+
+/* GET home page. */
+router.get("/", async function (req, res, next) {
+  let user = req.session.user;
+  console.log(user);
+  let cartCount = null;
+  if (req.session.user) {
+    cartCount = await userHelpers.GetCartCount(req.session.user._id);
+  }
+  productHelpers.getAllProduct().then((products) => {
+    res.render("user/view-products", {
+      admin: false,
+      products,
+      user,
+      cartCount,
+    });
+  });
+});
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+  } else {
+    res.render("user/login", { loginErr: req.session.loginErr });
+    req.session.loginErr = false;
+  }
+});
+router.get("/signup", (req, res) => {
+  res.render("user/signup");
+});
+router.post("/signup", (req, res) => {
+  userHelpers.doSignup(req.body).then((response) => {
+    console.log(response);
+    req.session.loggedIn = true;
+    req.session.user = response;
+    res.redirect("/");
+  });
+});
+router.post("/login", (req, res) => {
+  userHelpers.doLogin(req.body).then((response) => {
+    if (response.status) {
+      req.session.loggedIn = true;
+      req.session.user = response.user;
+      res.redirect("/");
+    } else {
+      req.session.loginErr = "invalid emailid or password";
+      res.redirect("/login");
+    }
+  });
+});
+router.get("/logout", (req, res) => {
+  req.session.destroy(function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/");
+    }
+  });
+});
+
+router.get("/cart", verifyLogin, async (req, res) => {
+  let products = await userHelpers.getCartProducts(req.session.user._id);
+  console.log(products);
+  res.render("user/cart", { products, user: req.session.user });
+});
+router.get("/add-to-cart/:id", (req, res) => {
+  console.log("api call");
+  userHelpers.addToCart(req.params.id, req.session.user._id).then(() => {
+    // res.redirect("/");  ajax use cheynd add to cart clickil
+    res.json({ status: true });
+  });
+});
+router.post("/change-product-quantity", (req, res) => {
+  userHelpers.changeProductQuantity(req.body).then((response) => {
+    res.json(response);
+  });
+});
+router.post("/remove-product", (req, res) => {
+  console.log("hai");
+  userHelpers.removeProduct(req.body).then((response) => {
+    res.json(response);
+  });
+});
+router.get("/place-order", (req, res) => {
+  res.render("user/place-order");
+});
+
+module.exports = router;
